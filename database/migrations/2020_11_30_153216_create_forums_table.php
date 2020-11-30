726644a60563bd313c65798b88b65d7764bfb43a
o-oo-ooo <ooo-oo-o@qq.com>
2020-11-30 23:49:53 +0800

    add forum table and seeder

---
+++ database/migrations/2020_11_30_153216_create_forums_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateForumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forums', function (Blueprint $table) {
+            $table->id();
+            $table->char('name', 16);
+            $table->unsignedTinyInteger('rank')->default(0);
+            $table->unsignedMediumInteger('threads')->default(0);
+            $table->unsignedMediumInteger('today_posts')->default(0);
+            $table->unsignedMediumInteger('today_threads')->default(0);
+            $table->text('brief');
+            $table->text('announcement');
+            $table->unsignedInteger('accesson')->default(0);
+            $table->unsignedTinyInteger('orderby')->default(0);
+            $table->unsignedInteger('icon_create_at')->default(0);
+            $table->char('moduids', 120);
+            $table->char('seo_title', 64);
+            $table->char('seo_keywords', 64);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forums');
+    }
+}
---
+++ database/seeders/ForumSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ForumSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('forums')->insert([
+            'name' => '默认板块',
+            'brief' => '默认板块介绍'
+        ]);
+    }
+}
