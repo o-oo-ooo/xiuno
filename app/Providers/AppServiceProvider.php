119ebb5d6f2dc33e065e470f1d3c6a579fabca42
o-oo-ooo <ooo-oo-o@qq.com>
2020-12-02 23:28:15 +0800

    add share variable in the template

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Forum;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 导航栏栏目列表
+        $group_id = Auth::user() ? Auth::user()->group_id : 0;
+        $forums = Forum::all()->reject(function ($value) {
+            return $value->accesson && !$value->accesslist[$group_id]['allowread'];
+        });
+        
+        View::share('forumlist', $forums);
+        View::share('forumarr', $forums->pluck('name', 'id')->toJson(JSON_UNESCAPED_UNICODE));
     }
 }
