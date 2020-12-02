428c9df58f4a683139db5a9ef558e463054c34e5
o-oo-ooo <ooo-oo-o@qq.com>
2020-12-02 23:31:42 +0800

    add accessors for forum model

--- app/Models/Forum.php
+++ app/Models/Forum.php
@@ -8,4 +8,44 @@ use Illuminate\Database\Eloquent\Model;
 class Forum extends Model
 {
     use HasFactory;
+    
+    /**
+     * 获取与栏目相关的访问规则
+     *
+     * @return array
+     */
+    public function forumAccess()
+    {
+        return $this->hasMany(ForumAccess::class);
+    }
+    
+    /**
+     * 获取论坛icon
+     */
+    public function getIconAttribute($value)
+    {
+        return $value ? asset('forum/' . $this->id . ',png') : asset('images/forum.png');
+    }
+    
+    /**
+     * 获得板块访问列表
+     *
+     * @return array
+     */
+    public function getAccesslistAttribute($value)
+    {
+        return $this->accesson ? $this->forumAccess() : [];
+    }
+    
+    /**
+     * 获取板块的版主列表
+     *
+     * @return array
+     */
+    public function getModuidsAttribute($value)
+    {
+        return $value ? User::find(explode(',', $value)) : [];
+    }
+    
+    
 }
